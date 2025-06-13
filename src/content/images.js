921bf1ef5df257743d5ba6f14bf7f9fa1552c693
David Susairaj <david.susairaj@gmail.com>
2025-06-13 19:24:14 +0530

    nature image added

--- src/content/images.js
+++ src/content/images.js
@@ -1,9 +1,24 @@
 import tulasi from '../images/tulasiImg.png';
 import munnurai_1 from '../images/munnurai_1.png';
 import diaryWriten from '../images/diaryWriten.png';
+import nature_peace from '../images/nature_peace.png';
+import tulsi_gift from '../images/tulsi_gift.png';
+import nature_lap from '../images/nature_lap.png';
+import parents_love from '../images/parents_love.png';
+import parents_care from '../images/parents_care.png';
+import parents_age from '../images/parents_age.png';
+import parents_root from '../images/parents_root.png';
 
 export const images = {
-  tulasi,
   munnurai_1,
-  diaryWriten
+  diaryWriten,
+  tulasi,
+  nature_peace,
+  tulsi_gift,
+  nature_lap,
+  parents_love,
+  parents_care, 
+  parents_age,
+  parents_root
+
 };
